8db8f3975f2fb7f6db32a85ca8fdc63740356dad
ofca <ofca@emve.org>
2023-09-19 21:48:05 +0200

    Fixes to git_log.sh.

--- ee/git_log.sh
+++ ee/git_log.sh
@@ -2,11 +2,11 @@
 
 set -eo pipefail
 
-cwd="$1"
 action="$1"
 
 if [ "$action" == "help" ]; then
-  echo 'hello world'
+  echo 'Print prettier git log.'
+  exit 0
   fi
 
-cd "$cwd" && git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all
+git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all
